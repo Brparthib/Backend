1b86819b9693dbc6e5f8a77b058f927f33353f65
ParthibBr <bparthib77@gmail.com>
2024-07-26 11:40:26 +0600

    util code

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 require("./db/dbConnect.js");
 require("dotenv").config();
+const port = process.env.PORT || 4000;
+const app = express();
+
+// middleware
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// listening server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
